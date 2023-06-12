84a44c7b73c6bf0a5089b056013a45caa76e6c1f
ryuzakiakira <ryuzakiakira@protonmail.com>
2023-06-12 11:49:29 +0530

    done change color of div on mouseover

--- js/script.js
+++ js/script.js
@@ -2,10 +2,22 @@
 
 function createDiv() {
     const grid = document.querySelector('.grid')
-    for (let i = 0; i <= 255; i++) {
-        let div = document.createElement('div')
-        grid.appendChild(div)
+    for (let i = 1; i <= 256; i++) {
+        let div = document.createElement('div');
+        div.setAttribute('id', `div${i}`);
+        grid.appendChild(div);
     };
 }
 
 createDiv()
+
+// change the background color of the divs when hovering over them
+
+function changeColor(e) {
+    let div = document.getElementById(`${e.target.id}`);
+    div.style.cssText = 'background-color: black;'
+}
+
+window.addEventListener('mouseover', function(event){
+    changeColor(event);
+})
